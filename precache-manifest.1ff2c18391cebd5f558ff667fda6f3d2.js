904e5c1e707f792877acdb61c693fac286c8b324
Anthony Mwangi <tony.subscryb@gmail.com>
2020-06-20 16:06:58 +0300

    Updates

---
+++ precache-manifest.1ff2c18391cebd5f558ff667fda6f3d2.js
@@ -0,0 +1,98 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4695f82defe155461c872008941e1977",
+    "url": "/Travlo/index.html"
+  },
+  {
+    "revision": "132defe154d30bcf510e",
+    "url": "/Travlo/static/css/main.5770a396.chunk.css"
+  },
+  {
+    "revision": "287462e3617ffb785e34",
+    "url": "/Travlo/static/js/2.ef9ce7b9.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/Travlo/static/js/2.ef9ce7b9.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "132defe154d30bcf510e",
+    "url": "/Travlo/static/js/main.d6045f5f.chunk.js"
+  },
+  {
+    "revision": "d9f6637820518bd6d5d2",
+    "url": "/Travlo/static/js/runtime-main.f6048c96.js"
+  },
+  {
+    "revision": "4e1c7a2d0278fa3dbd2d1ef5a9fd8a37",
+    "url": "/Travlo/static/media/add_btn.4e1c7a2d.svg"
+  },
+  {
+    "revision": "40e756d3ccc676d8dc83a4d20bac1164",
+    "url": "/Travlo/static/media/airplane.40e756d3.svg"
+  },
+  {
+    "revision": "124273c55a0120570390c520a8e6df4c",
+    "url": "/Travlo/static/media/book-button.124273c5.svg"
+  },
+  {
+    "revision": "47e802ff9be8959844b71b6ba87fa901",
+    "url": "/Travlo/static/media/checkout-tab.47e802ff.svg"
+  },
+  {
+    "revision": "39282aaac67e56f02f55693ebb141d83",
+    "url": "/Travlo/static/media/component-1.39282aaa.svg"
+  },
+  {
+    "revision": "74f1052a0e0a07a94737bde5dc1e262f",
+    "url": "/Travlo/static/media/component-2.74f1052a.svg"
+  },
+  {
+    "revision": "cefc850035494b666c171660026da4a1",
+    "url": "/Travlo/static/media/cta-icon.cefc8500.svg"
+  },
+  {
+    "revision": "07ca1817eaa042a9c573a8a8ad23a17c",
+    "url": "/Travlo/static/media/flight-tab.07ca1817.svg"
+  },
+  {
+    "revision": "c7ae355e3d72f1c7cacc3ae60e7a08b0",
+    "url": "/Travlo/static/media/hotel-tab.c7ae355e.svg"
+  },
+  {
+    "revision": "63546b0fb0795fcef00b6af30067b995",
+    "url": "/Travlo/static/media/main-text.63546b0f.svg"
+  },
+  {
+    "revision": "7f05c06f6d8ad8b808b76079b10ba1f8",
+    "url": "/Travlo/static/media/menu.7f05c06f.svg"
+  },
+  {
+    "revision": "8c5473f1b1dff4132f6a93fcf4cd3ea9",
+    "url": "/Travlo/static/media/play-button.8c5473f1.svg"
+  },
+  {
+    "revision": "e3502f2fe9ed4e5c5dc71c275ed17a71",
+    "url": "/Travlo/static/media/seats-tab.e3502f2f.svg"
+  },
+  {
+    "revision": "844fa5a8c3293c0f1a986519d56824fd",
+    "url": "/Travlo/static/media/section-2.844fa5a8.png"
+  },
+  {
+    "revision": "47f692d27f0e875fabfb1519584e8646",
+    "url": "/Travlo/static/media/section-3.47f692d2.png"
+  },
+  {
+    "revision": "b4d546e51ad8a13c969f80e6e9376ed9",
+    "url": "/Travlo/static/media/section-4.b4d546e5.png"
+  },
+  {
+    "revision": "0c36d674e4937d0278e45a8da2da666f",
+    "url": "/Travlo/static/media/travels-tab.0c36d674.svg"
+  },
+  {
+    "revision": "ddab56c603564f308ed4fe5b589974ac",
+    "url": "/Travlo/static/media/world-map.ddab56c6.png"
+  }
+]);
